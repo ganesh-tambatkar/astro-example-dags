76932185b21cbcf0f5c33f890ec965cd50306ab8
Ganesh Tambatkar <72242467+ganesh-tambatkar@users.noreply.github.com>
2023-10-18 15:02:15 +0530

    Update s3_operations_and_transformation.py

--- dags/s3_operations_and_transformation.py
+++ dags/s3_operations_and_transformation.py
@@ -2,6 +2,7 @@ import os
 from airflow import DAG
 from time import time_ns
 from datetime import datetime
+from airflow.hooks.S3_hook import S3Hook
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
