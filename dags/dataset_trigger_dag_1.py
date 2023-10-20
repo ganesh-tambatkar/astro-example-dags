4e7baf3137e2866d849efd2e5ad8badc51cf1afb
Ganesh Tambatkar <72242467+ganesh-tambatkar@users.noreply.github.com>
2023-10-20 16:11:35 +0530

    Update dataset_trigger_dag_1.py

--- dags/dataset_trigger_dag_1.py
+++ dags/dataset_trigger_dag_1.py
@@ -1,9 +1,13 @@
+import time
 from airflow import DAG
 from airflow.datasets import Dataset
 from datetime import datetime, timedelta
 from airflow.operators.empty import EmptyOperator
 
 dataset_1 = Dataset("s3://dataset-bucket/example.csv")
+
+def sleep_fun():
+    time.sleep(30)
 
 with DAG(
     dag_id="dataset_trigger_dag_1", schedule=None, start_date=datetime(2023, 1, 1), is_paused_upon_creation=False, catchup=False
@@ -14,5 +18,10 @@ with DAG(
         doc_md="""Dummy Start Task""",
         outlets=[dataset_1]
     )
+
+    t_sleep = PythonOperator(
+        task_id='sleep',
+        python_callable=sleep_fun
+    )
     
-    t_start
+    t_start >> t_sleep
