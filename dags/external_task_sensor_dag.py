0f7ce9853bb6aba938c44036590df0ddd1013fcd
Ganesh Tambatkar <72242467+ganesh-tambatkar@users.noreply.github.com>
2023-10-20 13:54:19 +0530

    Update external_task_sensor_dag.py

--- dags/external_task_sensor_dag.py
+++ dags/external_task_sensor_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow.operators.empty import EmptyOperator
 from airflow.sensors.external_task import ExternalTaskMarker, ExternalTaskSensor
 
