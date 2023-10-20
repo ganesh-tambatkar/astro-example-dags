faef55eac4fb2c0efa7afcb766a52f0b4f990c1a
Ganesh Tambatkar <72242467+ganesh-tambatkar@users.noreply.github.com>
2023-10-20 13:36:43 +0530

    Create external_task_sensor_dag.py

---
+++ dags/external_task_sensor_dag.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.sensors.external_task import ExternalTaskMarker, ExternalTaskSensor
+
+with DAG(
+    dag_id="external_task_sensor_dag", schedule=None, start_date=datetime(2023, 1, 1), is_paused_upon_creation=False, catchup=False
+) as dag:
+    
+    t_start = EmptyOperator(
+        task_id='Start',
+        doc_md="""Dummy Start Task"""
+    )
+
+    t_external_task_sensor = ExternalTaskSensor(
+        task_id="parent_task_sensor",
+        external_task_id="trigger_dependent_dag",
+        external_dag_id="DAG1",
+        deferrable=True,
+    )
+
+    t_end = EmptyOperator(
+        task_id='End',
+        doc_md="""Dummy End Task"""
+    )
+
+    t_start >> t_external_task_sensor >> t_end
+
+    
