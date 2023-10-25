9e50d336d9390d5dfc75dfee5d773f4d2e8e5433
Ganesh Tambatkar <72242467+ganesh-tambatkar@users.noreply.github.com>
2023-10-25 17:04:30 +0530

    Create mutual_exclusion_dag.py

---
+++ dags/mutual_exclusion_dag.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.models.dagbag import DagBag
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+def update_dag_state():
+    dag_bag = DagBag(read_dags_from_db=False)
+    for dag_id_ in dag_bag.dag_ids:
+        print("dag_id =>", dag_id_)
+
+with DAG(
+    dag_id="DAG1", schedule=None, start_date=datetime(2023, 10, 25), is_paused_upon_creation=False, catchup=False
+) as dag:
+
+    start_task = DummyOperator(task_id='start_task', dag=dag)
+    
+    task_1 = PythonOperator(
+        task_id = 'task_1',
+        python_callable = update_dag_state
+    )
+    
+    start_task >> task_1
