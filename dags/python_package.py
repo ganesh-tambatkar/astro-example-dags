d3ec73a637bb5089c0ead0d05adde865ad338896
Ganesh Tambatkar <72242467+ganesh-tambatkar@users.noreply.github.com>
2023-11-01 12:18:52 +0530

    Create python_package.py

---
+++ dags/python_package.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from airflow import DAG
+from datetime import datetime
+
+with DAG(
+    dag_id="python_package", schedule=None, start_date=datetime(2023, 1, 1), is_paused_upon_creation=False, catchup=False
+) as dag:
+  
+    t_start = EmptyOperator(
+        task_id='Start',
+        doc_md="""Dummy Start Task"""
+    )
+
+    t_start
