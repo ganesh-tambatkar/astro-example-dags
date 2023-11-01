93a69aa179cc1a5225f5497bafbe96fd99cf3a01
Ganesh Tambatkar <72242467+ganesh-tambatkar@users.noreply.github.com>
2023-11-01 12:20:36 +0530

    Update python_package.py

--- dags/python_package.py
+++ dags/python_package.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from airflow import DAG
 from datetime import datetime
+from airflow.operators.empty import EmptyOperator
 
 with DAG(
     dag_id="python_package", schedule=None, start_date=datetime(2023, 1, 1), is_paused_upon_creation=False, catchup=False
