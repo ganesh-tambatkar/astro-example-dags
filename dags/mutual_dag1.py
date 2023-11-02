3413e4a7925ab4fc297e3892426e7720254af5a1
Ganesh Tambatkar <72242467+ganesh-tambatkar@users.noreply.github.com>
2023-11-02 16:20:43 +0530

    Update mutual_dag1.py

--- dags/mutual_dag1.py
+++ dags/mutual_dag1.py
@@ -1,8 +1,14 @@
+import time
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
 from airflow.providers.amazon.aws.operators.s3 import S3CreateObjectOperator, S3DeleteObjectsOperator
+
+def raw_to_prep_and_prep_to_red:
+    time.sleep(60)
+    return True
 
 with DAG(
     dag_id="mutual_dag1", schedule=None, start_date=datetime(2023, 11, 2), is_paused_upon_creation=False, catchup=False
@@ -17,7 +23,8 @@ with DAG(
             's3://aws-cloudtrail-logs-493179717493-0fc50be1/temp/astro-demos-sample-data/trigger_file.txt'
         ],
         aws_conn_id='aws_conn',
-        timeout=30
+        poke_interval=30,
+        timeout=300
     )
 
     mutual_exclusion_file_sensor_task = S3KeySensor(
@@ -27,7 +34,8 @@ with DAG(
             's3://aws-cloudtrail-logs-493179717493-0fc50be1/temp/astro-demos-sample-data/mutual_exclusion_file.txt'
         ],
         aws_conn_id='aws_conn',
-        timeout=30
+        poke_interval=30,
+        timeout=300
     )
 
     delete_mutual_exclusion_file_task = S3DeleteObjectsOperator(
@@ -39,7 +47,10 @@ with DAG(
       aws_conn_id='aws_conn'
     )
 
-    raw_to_prep_and_prep_to_red_task = DummyOperator(task_id='raw_to_prep_and_prep_to_red_task', dag=dag)
+    raw_to_prep_and_prep_to_red_task = PythonOperator(
+        task_id='raw_to_prep_and_prep_to_red_task',
+        python_callable=dag,
+    )
 
     create_mutual_exclusion_file_task = S3CreateObjectOperator(
         task_id="create_mutual_exclusion_file",
