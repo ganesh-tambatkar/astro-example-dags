212672026c62775a0c3ddf5db4f9ea12a693ba66
Ganesh Tambatkar <72242467+ganesh-tambatkar@users.noreply.github.com>
2024-01-19 12:37:36 +0530

    Update second.py

--- dags/second.py
+++ dags/second.py
@@ -6,16 +6,16 @@ from airflow.sensors.external_task_sensor import ExternalTaskSensor
 from datetime import datetime, timedelta
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-args = {“owner”: “airflow”, “start_date”: airflow.utils.dates.days_ago(1)}
+args = {"owner": "airflow", "start_date": airflow.utils.dates.days_ago(1)}
 
 dag = DAG(
- dag_id=”Second”, default_args=args, schedule_interval=’55 06 * * *’
+ dag_id="Second", default_args=args, schedule_interval="55 06 * * *"
 )
 def pp():
  print(‘Second Dependent Task’)
  
 with dag:
- Second_Task=PythonOperator(task_id=”Second_Task”, python_callable=pp,dag=dag)
+ Second_Task=PythonOperator(task_id="Second_Task", python_callable=pp,dag=dag)
 ExternalTaskSensor(
  task_id=’Ext_Sensor_Task’,
  external_dag_id=’First’,
