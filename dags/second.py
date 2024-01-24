bf914550920123e5f3a396dcdfada72e42cda02c
Ganesh Tambatkar <72242467+ganesh-tambatkar@users.noreply.github.com>
2024-01-24 16:26:18 +0530

    Update second.py

--- dags/second.py
+++ dags/second.py
@@ -17,12 +17,10 @@ def pp():
  
 with dag:
  Second_Task=PythonOperator(task_id="Second_Task", python_callable=pp,dag=dag)
-# ExternalTaskSensor(
-#  task_id="Ext_Sensor_Task",
-#  external_dag_id="first",
-#  external_task_id="first_task",
-#  # execution_delta = timedelta(minutes=10),
-#  timeout=300,
-#  dag=dag)>>Second_Task
-
-Second_Task
+ExternalTaskSensor(
+ task_id="Ext_Sensor_Task",
+ external_dag_id="first",
+ external_task_id="first_task",
+ execution_delta = datetime.date.today(),
+ timeout=300,
+ dag=dag)>>Second_Task
