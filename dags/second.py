082bb7b6fbbe5a82593f3d984cb8bbafe4958e44
Ganesh Tambatkar <72242467+ganesh-tambatkar@users.noreply.github.com>
2024-01-24 16:30:03 +0530

    Update second.py

--- dags/second.py
+++ dags/second.py
@@ -3,7 +3,7 @@ import airflow
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.sensors.external_task_sensor import ExternalTaskSensor
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 args = {"owner": "airflow", "start_date": airflow.utils.dates.days_ago(1)}
@@ -21,6 +21,6 @@ ExternalTaskSensor(
  task_id="Ext_Sensor_Task",
  external_dag_id="first",
  external_task_id="first_task",
- execution_delta = datetime.date.today(),
+ execution_delta = date.today(),
  timeout=300,
  dag=dag)>>Second_Task
